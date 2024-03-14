d1b6c396d71afa8825e5fb1192f790c30ecaa838
lingfeng <332373135@qq.com>
2024-03-14 21:39:33 +0800

    add sass and button test

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
-<script setup>
-</script>
-
 <template>
-
+  <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">按钮</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
 </template>
 
-<style scoped>
+<script lang="ts" setup>
 
+</script>
+
+<style scoped>
+  button:focus {
+    outline: none; /*解决浏览器黑色边框问题*/
+  }
 </style>
