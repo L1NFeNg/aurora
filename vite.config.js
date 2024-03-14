e3eab6eb1b742d45856db995ccdfed699d9d419d
lingfeng <332373135@qq.com>
2024-03-14 21:57:59 +0800

    add rollup-plugin-visualizer

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,11 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()]
+    }),
+    visualizer({
+      // 打包体积分析
+      open: true,
+      filename: "visualizer.html" //分析图生成的文件名
     })
   ]
 });
