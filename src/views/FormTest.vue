98567c487fef17e85add083a9add7d720726a211
lingfeng <332373135@qq.com>
2024-03-18 20:57:18 +0800

    尝试封装节流防抖功能在按钮组件中-update

--- src/views/FormTest.vue
+++ src/views/FormTest.vue
@@ -3,8 +3,9 @@
   import { ButtonAbleModeEnum } from "@/enums/form/formEnum.ts";
 
 
-  const fn = (n: number) => {
+  const fn = (n: any, event?: Event) => {
     console.log("按钮点击事件触发了！" + n);
+    console.log(event);
   };
 
 </script>
@@ -18,7 +19,8 @@
     <Button :ableMode="ButtonAbleModeEnum.ONCE" :handle-function="fn">审核</Button>
     <Button :handle-function="fn">驳回</Button>
     <Button :handle-function="fn">删除</Button>
-    <Button :ableMode="ButtonAbleModeEnum.DEBOUNCE" :handle-function="()=>console.log(1)">防抖</Button>
+    <Button :ableMode="ButtonAbleModeEnum.DEBOUNCE" @click="fn(1,$event)" :handle-function="()=>console.log(1)">防抖
+    </Button>
     <Button :ableMode="ButtonAbleModeEnum.THROTTLE" :handle-function="fn">节流</Button>
   </div>
 </template>
